a2ce1f385375e6bb074d448295f550f304c836e6
Naman Tamrakar <namantam1@gmail.com>
2023-04-22 00:42:49 +0530

    Format code to 2 tab

--- src/main/java/namantam1/ChatProducer.java
+++ src/main/java/namantam1/ChatProducer.java
@@ -8,22 +8,22 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import java.util.Properties;
 
 public class ChatProducer {
-    public static final String BOOTSTRAP_SERVER = "pkc-6ojv2.us-west4.gcp.confluent.cloud:9092";
+  public static final String BOOTSTRAP_SERVER = "pkc-6ojv2.us-west4.gcp.confluent.cloud:9092";
 
-    public static void main(String[] args) {
-        // setup config
-        Properties config = new Properties();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put("security.protocol", "SASL_SSL");
-        config.put("sasl.mechanism", "PLAIN");
+  public static void main(String[] args) {
+    // setup config
+    Properties config = new Properties();
+    config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+    config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    config.put("security.protocol", "SASL_SSL");
+    config.put("sasl.mechanism", "PLAIN");
 
-        // setup producer
-        KafkaProducer<String, String> producer = new KafkaProducer<>(config);
+    // setup producer
+    KafkaProducer<String, String> producer = new KafkaProducer<>(config);
 
-        producer.send(new ProducerRecord<>("chat-topic", "hi!!!"));
+    producer.send(new ProducerRecord<>("chat-topic", "hi!!!"));
 
-        producer.close();
-    }
+    producer.close();
+  }
 }
--- src/main/java/namantam1/KafkaChatConsumer.java
+++ src/main/java/namantam1/KafkaChatConsumer.java
@@ -11,31 +11,31 @@ import java.util.Properties;
 
 public class KafkaChatConsumer {
 
-    public static final String BOOTSTRAP_SERVER = "pkc-6ojv2.us-west4.gcp.confluent.cloud:9092";
-    public static final String CONSUMER_GROUP_ID = "ndfbtwex-KAFKA_CHAR_CONSUMER-1";
+  public static final String BOOTSTRAP_SERVER = "pkc-6ojv2.us-west4.gcp.confluent.cloud:9092";
+  public static final String CONSUMER_GROUP_ID = "ndfbtwex-KAFKA_CHAR_CONSUMER-1";
 
-    public static void main(String[] args) {
-        // setup config
-        Properties config = new Properties();
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, CONSUMER_GROUP_ID);
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
-        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
-        config.put("security.protocol", "SASL_SSL");
-        config.put("sasl.mechanism", "PLAIN");
+  public static void main(String[] args) {
+    // setup config
+    Properties config = new Properties();
+    config.put(ConsumerConfig.GROUP_ID_CONFIG, CONSUMER_GROUP_ID);
+    config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+    config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+    config.put("security.protocol", "SASL_SSL");
+    config.put("sasl.mechanism", "PLAIN");
 //        config.put("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required " +
 //                "username='' password='';");
 
-        // setup consumer
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(config);
-        consumer.subscribe(List.of("chat-topic"));
+    // setup consumer
+    KafkaConsumer<String, String> consumer = new KafkaConsumer<>(config);
+    consumer.subscribe(List.of("chat-topic"));
 
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-            for (var record : records) {
-                System.out.printf("key = %s, value = %s%n", record.key(), record.value());
-            }
-        }
+    while (true) {
+      ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+      for (var record : records) {
+        System.out.printf("key = %s, value = %s%n", record.key(), record.value());
+      }
     }
+  }
 }
